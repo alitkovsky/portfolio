744e00ab1be605fe7a0cf02c8767f5544e2aa3ed
alitkovsky <alitkovsky@me.com>
2024-05-29 14:59:24 +0200

    Rebuild a Next.js app wirt required dependencies for my portfolio.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,22 @@
-import type { Config } from "tailwindcss"
-
-const config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
   ],
   prefix: "",
   theme: {
     container: {
       center: true,
-      padding: "15px",
-    },
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "960px",
-      xl: "1200px",
-    },
-    fontFamily: {
-      primary: "var(--font-fira-code), system-ui, sans-serif",
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
     extend: {
-      colors: {
-        primary: "#1c1c22",
-        accent: {
-          DEFAULT: "#00FF99",
-          hover: "#00e187",
-        }
-      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -48,6 +34,4 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
-
-export default config
+}
